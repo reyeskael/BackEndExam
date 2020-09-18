53aec8bf0776b9c81606c5c96a56d4dd90cb38fd
reyeskael <michaelreyes0202@gmail.com>
2020-09-19 02:27:57 +0800

    Includes database creation

--- backendexam_tinformation.sql
+++ backendexam_tinformation.sql
@@ -1,3 +1,5 @@
+CREATE DATABASE  IF NOT EXISTS `backendexam` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `backendexam`;
 -- MySQL dump 10.13  Distrib 8.0.21, for Win64 (x86_64)
 --
 -- Host: 127.0.0.1    Database: backendexam
@@ -50,4 +52,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2020-09-18 23:51:08
+-- Dump completed on 2020-09-19  2:26:28
